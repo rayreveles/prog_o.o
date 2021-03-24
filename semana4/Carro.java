c275078cee8b12f1baf9ae539e78beb9deb8bcc2
rayreveles <79915954+rayreveles@users.noreply.github.com>
2021-03-24 11:46:20 -0300

    Update Carro.java

--- semana4/Carro.java
+++ semana4/Carro.java
@@ -1,1 +1,25 @@
+public class Carro {
 
+    private int num_acentos;
+    private String cor;
+
+    public int getNum_acentos(){
+        return num_acentos;
+       }
+  
+  
+    public void setNum_acentos(int num_acentos){
+        this.num_acentos = num_acentos;
+       }
+ 
+  
+    public String getCor(){
+        return cor;
+       }
+  
+  
+    public void setCor(String cor){
+        this.cor = cor;
+       }
+    
+ }
