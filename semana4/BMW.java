c68c9f849a57af2ea34a9dd309ca65b5ea4f54c1
rayreveles <79915954+rayreveles@users.noreply.github.com>
2021-03-24 11:54:23 -0300

    Update BMW.java

--- semana4/BMW.java
+++ semana4/BMW.java
@@ -13,6 +13,6 @@ public class BMW extends Carro {
         return velocidade;
     }
     public void imprimir(){
-       System.out.printf("BMW de cor: %s, com %d acentos e velocidade %.2f",getCor(),getNum_acentos(),velocidade);
+       System.out.printf("BMW de cor: %s, possui: %d acentos e velocidade: %.2f",getCor(),getNum_acentos(),velocidade);
     }  
 }
