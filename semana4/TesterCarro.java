222f51dfdf466906a5a7ccd0d84f8a175760bbbb
rayreveles <79915954+rayreveles@users.noreply.github.com>
2021-03-24 11:53:18 -0300

    Update TesterCarro.java

--- semana4/TesterCarro.java
+++ semana4/TesterCarro.java
@@ -1,1 +1,10 @@
+public class TesterCarro {
 
+    public static void main(String[] args){
+
+        BMW carro = new BMW(70.8);
+
+       carro.setCor("Preto");
+       carro.setNum_acentos(5);
+       carro.imprimir();
+    }
