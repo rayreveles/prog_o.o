f11f95bfbdcbc76baa72dc516949b52246b5ece3
rayreveles <79915954+rayreveles@users.noreply.github.com>
2021-04-15 23:22:55 -0300

    Create Programa.java

---
+++ semana 6/Programa.java
@@ -0,0 +1,26 @@
+package principal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entidades.*;
+import util.UtilFormas;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Circulo circulo = new Circulo( UtilFormas.ler());
+		
+		Quadrado quadrado = new Quadrado(UtilFormas.ler());
+		
+		List<Forma> formas = new ArrayList<>();
+		
+		formas.add(circulo);
+		formas.add(quadrado);
+		
+	UtilFormas.imprimir(formas);
+	UtilFormas.imprimir(circulo);
+	}
+}
